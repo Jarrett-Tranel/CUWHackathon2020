ec2efd53a39cad73567d3cdd2938b1d4e4881853
Jarrett Tranel <49590631+DoubleShotTranel@users.noreply.github.com>
2020-11-07 10:57:24 -0600

    Update CUWHackathon2020.py

--- CUWHackathon2020.py
+++ CUWHackathon2020.py
@@ -21,14 +21,35 @@ done = False
 rect_x = 50
 rect_y = 50
 
+x_speed = 0
+y_speed = 0
+
+clock = pygame.time.Clock()
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_speed = -50
+            elif event.key == pygame.K_RIGHT:
+                x_speed = 50
+            elif event.key == pygame.K_UP:
+                y_speed = -50
+            elif event.key == pygame.K_DOWN:
+                y_speed = 50
+    
+    rect_x = rect_x + x_speed
+    rect_y = rect_y + y_speed
+    x_speed = 0
+    y_speed = 0
     screen.fill(BLACK)
 
     pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 50, 50])
     pygame.draw.rect(screen, RED, [rect_x + 10, rect_y + 10, 30, 30])
+    pygame.draw.circle(screen, RED, [rect_x + 100, rect_y + 100], 30)
+
 
     pygame.display.flip()
+    clock.tick(60)
