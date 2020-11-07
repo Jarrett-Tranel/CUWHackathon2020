d36713a4febf0f264e52c8fd62ad880e62503752
Jarrett Tranel <49590631+DoubleShotTranel@users.noreply.github.com>
2020-11-07 15:58:13 -0600

    YouCanWinNow

--- CUWHackathon2020.py
+++ CUWHackathon2020.py
@@ -38,6 +38,7 @@ pygame.display.set_caption('Show Text')
 
 
 #random.seed(30)
+#rocks
 for i in range(0, 10):
      for j in range(0, 10):
         ranNum = random.randint(0, 3)
@@ -57,6 +58,11 @@ randy = random.randint(0, 9)
 twoDArray[randx][randy] = 3
 enemylocation = [randx, randy]
 
+#player
+twoDArray[4][4] = 4
+playerlocation = [4,4]
+tempplayerlocation = [4,4]
+
 
 X = 700
 Y = 500
@@ -65,6 +71,7 @@ size = [X, Y]
 
 
 screen = pygame.display.set_mode(size)
+#winScreen = pygame.display.set_mode([300,400])
 
 font = pygame.font.Font('freesansbold.ttf', 32) 
 
@@ -72,18 +79,22 @@ titleText = font.render('MAZE', True, GREY, scoreTextblue)
 titleText2 = font.render('ESCAPE', True, GREY, scoreTextblue) 
 text = font.render('Score', True, scoreTextgreen, scoreTextblue) 
 scoreNumber = font.render("0",True,scoreTextgreen,scoreTextblue)
+winText = font.render("You Win" , True, GREY, scoreTextblue)
 
 titleRect = titleText.get_rect()
 titleRect2 = titleText2.get_rect() 
 textRect = text.get_rect() 
 scoreNumberTextRect = scoreNumber.get_rect()
+winScreenRect = winText.get_rect()
 
 # set the center of the rectangular object. 
 titleRect.center = (X -70, Y -450)
 titleRect2.center = (X -70, Y -410)
 textRect.center = (X -70, Y // 3)
 scoreNumberTextRect.center = (X - 70 , Y-250) 
+winScreenRect.center = (350,Y - 450)
 
+winStatus = False;
 #time.sleep(2)
 #pygame.mixer.music.stop()
 
@@ -101,6 +112,10 @@ y_speed = 0
 
 clock = pygame.time.Clock()
 
+sprite = pygame.image.load('D:\csResources\Smaller Projects\Hackathon\CUWHackathon2020\sprite.PNG')
+
+
+
 while not done: 
 
     for event in pygame.event.get():
@@ -108,13 +123,31 @@ while not done:
             done = True
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x_speed = -50
+                if not (twoDArray[playerlocation[0] - 1][playerlocation[1]] == 1):
+                    twoDArray[playerlocation[0]][playerlocation[1]] = 0
+                    twoDArray[playerlocation[0] - 1][playerlocation[1]] = 4
+                    playerlocation[0] = playerlocation[0] - 1
+                if (twoDArray[playerlocation[0] - 1][playerlocation[1]] == 2):
+                    winStatus = True;
+                    
             elif event.key == pygame.K_RIGHT:
-                x_speed = 50
+                if not (twoDArray[playerlocation[0] + 1][playerlocation[1]] == 1):
+                    twoDArray[playerlocation[0]][playerlocation[1]] = 0
+                    twoDArray[playerlocation[0] + 1][playerlocation[1]] = 4
+                    playerlocation[0] = playerlocation[0] + 1
+                if (twoDArray[playerlocation[0] + 1][playerlocation[1]] == 2):
+                    winStatus = True;
+ 
             elif event.key == pygame.K_UP:
-                y_speed = -50
+                if not (twoDArray[playerlocation[0]][playerlocation[1] - 1] == 1):
+                    twoDArray[playerlocation[0]][playerlocation[1]] = 0
+                    twoDArray[playerlocation[0]][playerlocation[1] - 1] = 4
+                    playerlocation[1] = playerlocation[1] - 1
             elif event.key == pygame.K_DOWN:
-                y_speed = 50
+                if not (twoDArray[playerlocation[0]][playerlocation[1] + 1] == 1):
+                    twoDArray[playerlocation[0]][playerlocation[1]] = 0
+                    twoDArray[playerlocation[0]][playerlocation[1] + 1] = 4
+                    playerlocation[1] = playerlocation[1] + 1
             else:
                 ranNum = random.randint(0, 1)
                 if (ranNum == 0):
@@ -129,7 +162,7 @@ while not done:
                     enemylocation[0] = enemylocation[0] + 1
                     
 
-              
+           
     
     rect_x = rect_x + x_speed
     rect_y = rect_y + y_speed
@@ -144,6 +177,8 @@ while not done:
                 colorToDraw = GREY
             elif twoDArray[i][j] == 2:
                 colorToDraw = LIGHTBLUE
+            elif twoDArray[i][j] == 4:
+                colorToDraw = WHITE
             pygame.draw.rect(screen, colorToDraw, [rect_x + (i * 50), rect_y + (j * 50), 40, 40])
 
   
@@ -151,7 +186,13 @@ while not done:
     screen.blit(scoreNumber, scoreNumberTextRect)
     screen.blit(titleText,titleRect)
     screen.blit(titleText2,titleRect2)
-    
+    #x=size[0]*.5
+    #y=size[1]*.5
+    #screen.blit(sprite, (x,y))
+    if (winStatus == True):
+        screen.fill(LIGHTBLUE)
+        #pygame.draw.rect( pygame.display.set_mode(size), LIGHTBLUE, [0, 0, size[X / 2], size[Y / 2]])
+        screen.blit(winText,winScreenRect )
     pygame.display.update()
     pygame.display.flip()
     clock.tick(60)
